d78a10cdfb10a8621fa28ac5b560f519e8c07e13
Joao Benevides Costa <joaobenevidescosta@hotmail.com>
2020-07-30 21:10:58 -0300

    Projeto De Importação de dados Para Woocomerce

---
+++ App Importacao/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace App_Importacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSiteurl = new System.Windows.Forms.TextBox();
+            this.txtSiteKey = new System.Windows.Forms.TextBox();
+            this.txtSiteSecret = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(430, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Site";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(334, 182);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(108, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar Processo";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 153);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.40566F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.59434F));
+            this.tableLayoutPanel1.Controls.Add(this.txtSiteSecret, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtSiteKey, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtSiteurl, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(424, 116);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Site:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Consumer key:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Consumer secret:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtSiteurl
+            // 
+            this.txtSiteurl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSiteurl.Location = new System.Drawing.Point(98, 3);
+            this.txtSiteurl.Name = "txtSiteurl";
+            this.txtSiteurl.Size = new System.Drawing.Size(323, 20);
+            this.txtSiteurl.TabIndex = 3;
+            // 
+            // txtSiteKey
+            // 
+            this.txtSiteKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSiteKey.Location = new System.Drawing.Point(98, 29);
+            this.txtSiteKey.Name = "txtSiteKey";
+            this.txtSiteKey.Size = new System.Drawing.Size(323, 20);
+            this.txtSiteKey.TabIndex = 4;
+            // 
+            // txtSiteSecret
+            // 
+            this.txtSiteSecret.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSiteSecret.Location = new System.Drawing.Point(98, 55);
+            this.txtSiteSecret.Name = "txtSiteSecret";
+            this.txtSiteSecret.Size = new System.Drawing.Size(323, 20);
+            this.txtSiteSecret.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 217);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Importação";
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtSiteSecret;
+        private System.Windows.Forms.TextBox txtSiteKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSiteurl;
+    }
+}
+
---
+++ App Importacao/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using WooCommerceNET;
+using WooCommerceNET.WooCommerce.v3;
+
+namespace App_Importacao
+{
+    public partial class Form1 : Form
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    //
+                    // Open the SqlConnection.
+                    //
+                    con.Open();
+                    //
+                    // This code uses an SqlCommand based on the SqlConnection.
+                    //
+                    using (SqlCommand command = new SqlCommand("SELECT * from planilha", con))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        var apirest = new RestAPI("http://developer.smmind.com.br/faina/wp-json/wc/v3/", "ck_d53f726ac27e7eeaa4ad4fba1d19cc7d73c98a43", "cs_e9fba980112026e7b451875af9acf2071f76dc55");
+                        var wc = new WCObject(apirest);
+
+                        while (reader.Read())
+                        {
+                            var produtoadd = new Product
+                            {
+
+                                //options ....
+                                type = "simple",
+                                manage_stock = true,
+                                name = reader["nomeProduto"].ToString(),
+                                description = reader["html"].ToString(),
+                                sku = reader["referencia"].ToString(),
+                                price = Convert.ToDecimal(reader["preco"].ToString()),
+                                regular_price = Convert.ToDecimal(reader["preco"].ToString()),
+                                sale_price = Convert.ToDecimal(reader["preco"].ToString()),
+                                images = BuscaImagem(Convert.ToInt32(reader["id"].ToString())),
+                                stock_quantity = 1
+                                
+                                //variations=varis,
+                            };
+
+                           var produtoadred = wc.Product.Add(produtoadd).Result;
+                        }
+                    }
+                }
+            
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(@"Tivemos um erro no metodo btnIniciar_Click:" + ex.Message );
+            }
+        }
+
+        private List<ProductImage> BuscaImagem(int? pkProduto)
+        {
+            var retorno = new List<ProductImage>();
+
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    con.Open();
+
+                    using (SqlCommand command = new SqlCommand("select * from PlanilhaFoto where entity_id=" + pkProduto, con))
+                    {
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                var imagem = new ProductImage
+                                {
+                                
+                                    src = dr["foto"].ToString(),
+                                };
+                                retorno.Add(imagem);
+                            }
+                        }
+                    }
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return retorno;
+        }
+    }
+}
